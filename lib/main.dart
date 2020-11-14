09f4ca107e7800144372bd3c2557279f88752b8e
Ryan Tillaman <37836505+yellow-cyber@users.noreply.github.com>
2020-11-14 23:17:38 +0800

    initial setup
    deleted test folder

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[900],
+        title: Text("World Time App"),
+      ),
+    );
+  }
+}
