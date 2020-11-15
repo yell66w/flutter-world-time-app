6296d2a821166a81c4a18ad6e6bde754ef436461
Ryan Tillaman <37836505+yellow-cyber@users.noreply.github.com>
2020-11-15 11:59:04 +0800

    changed home style

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,17 +13,38 @@ class _HomeState extends State<Home> {
     data = ModalRoute.of(context).settings.arguments;
     print(data);
     return Scaffold(
+      //appBar: AppBar(),
       body: SafeArea(
-        child: Column(
-          children: [
-            FlatButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/location');
-                },
-                icon: Icon(Icons.edit_location),
-                label: Text('Edit Location')),
-            Text(data['time'])
-          ],
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+          child: Column(
+            children: <Widget>[
+              FlatButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  icon: Icon(Icons.edit_location),
+                  label: Text('Edit Location')),
+              SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    data['location'],
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      letterSpacing: 2.0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.0),
+              Text(data['time'],
+                  style: TextStyle(
+                    fontSize: 66.0,
+                  )),
+            ],
+          ),
         ),
       ),
     );
